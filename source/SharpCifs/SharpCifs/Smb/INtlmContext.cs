fd1daf4a45f4aefa8d9d991fc7d4d68465087cec
Stephen Breen <breen.machine@gmail.com>
2016-01-16 12:36:02 -0500

    First commit

---
+++ source/SharpCifs/SharpCifs/Smb/INtlmContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCifs.Smb
+{
+
+    public interface INtlmContext {
+        bool IsEstablished();
+        byte[] GetServerChallenge();
+        byte[] GetSigningKey();
+        String GetNetbiosName();
+        byte[] InitSecContext(byte[] token, int offset, int len);
+    }
+
+}
